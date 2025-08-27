7d967e752ba2841177129819704a0da1559e99c8
JuanSavedra <juansavedra827@gmail.com>
2025-08-27 12:47:29 -0300

    getting started

---
+++ Estudos/FirstProgram.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() 
+{
+	int favorite_number;
+
+	std::cout << "Type your favorite number: ";
+	std::cin >> favorite_number;
+	std::cout << "Your favorite number is: " << favorite_number << ". Amazing!";
+
+	return 0;
+}
